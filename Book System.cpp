08b5aa667ab8f59d1fc2b6deb9755309896825e7
Sibusiso Nkabinde <1717564@students.wits.ac.za>
2021-03-25 21:00:41 +0200

    Latest updates on the project.

--- Book System.cpp
+++ Book System.cpp
@@ -1,57 +1,124 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class book {
 private:
-    string* author;
-    string* book_name;
-    string* publisher;
-    float* price;
-    int* stock;
+    string author;
+    string book_name;
+    string publisher;
+    float price;
+    int stock;
 public:
-    book() {
-        //author = new char[20];
-        //book_name = new char[20];
-        //publisher = new char[20];
-        author = new string;
-        book_name = new string;
-        publisher = new string;
-        price = new float;
-        stock = new int;
-    }
     void addbook() {
         cin.ignore();
         cout << "Please Enter the name of the Author :";
-        getline(cin,*author);
+        getline(cin,author);
         cout << "Please Enter the title name :";
-        getline(cin,*book_name);
+        getline(cin,book_name);
         cout << "Please Enter the name of the Publisher :";
-        getline(cin,*publisher);
+        getline(cin,publisher);
         cout << "Please Enter the price :";
-        cin >> *price;
+        cin >> price;
         cout << "Please Enter the stock position of the Book :";
-        cin >> *stock;
+        cin >> stock;
+    }
+    bool searchbook(string a, string b) {
+        if (book_name == a && author == b) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    void buybook() {
+        int copies;
+        cout << "Enter number of copies you wish to buy:";
+        cin >> copies;
+        if (copies <= stock) {
+            stock = stock - copies;
+            cout << "Book(s) Sucessfully bought . \n";
+            cout << "The amount paid in Rands is : " << price * copies << endl;
+        }
+        else {
+            cout << "Required number of copies not available . \n";
+        }
+    }
+    void showbook() {
+        cout << "Author: " << author << "\t" << "Title: " << book_name << "\t" << "Publisher: "  << publisher << "\t" << "price: " << price << "\t" <<"Stock position: "<< stock << endl;
+
     }
 
 };
 void menu() {
     cout << "\n\n\t\t\t\t\t Welcome to Heisenburg's Library . \n\n";
-    cout << "1. Add book to the library. \n";
-    cout << "2. Buy a Book. \n";
-    cout << "3. Search for a Book. \n";
-    cout << "4. Edit Details of Book. \n";
+    cout << "1. Add book to the library \n";
+    cout << "2. Buy a Book \n";
+    cout << "3. Search for a Book \n";
+    cout << "4. Edit Details of Book \n";
     cout << "5. Exit \n";
 }
 
-int main()
-{
-    int choice;
-    book obj1;
-    menu();
-    cin >> choice;
-    if (choice == 1) {
-        obj1.addbook();
+int main() {
+    vector < book* > obj1;
+    string writer, bookname;
+    int i = 0, q = 0;
+    while (true) {
+        menu();
+        int choice;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            obj1.push_back(new book);
+            obj1[i]->addbook();
+            i++;
+            cout << "Book Sucessfully Added ." << endl;
+            break;
+        case 2:
+            cout << "Enter the name of the book:";
+            cin.ignore();
+            getline(cin, bookname);
+            cout << "Enter the name of the Author:";
+            getline(cin, writer);
+            for (int k = 0;k < i;i++) {
+                if (obj1[k]->searchbook(bookname, writer) == true) {
+                    obj1[k]->buybook();
+                    q = 1;
+                    break;
+                }
+            }
+            if (q == 0) {
+                cout << " This book is not available .";
+            }
+                break;
+        case 3:
+            cout << "Enter the name of the book:";
+            cin.ignore();
+            getline(cin, bookname);
+            cout << "Enter the name of the Author:";
+            getline(cin, writer);
+            for (int k = 0;k < i;i++) {
+                if (obj1[k]->searchbook(bookname, writer) == true) {
+                    cout << "Book Found Sucessfully found . \n";
+                    obj1[k]->showbook();
+                    q = 1;
+                    break;
+                }
+            }
+            if (q == 0) {
+                cout << " This book is not available .";
+            }
+            break;
+        default:
+            break;
+            }
+
+        }
+
     }
-}
+        
+        
+    
